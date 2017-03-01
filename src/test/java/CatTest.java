e5b9742e12df4abec3f354f667db27229b28e8c7
tkemaladze <tkemaladze.k.a.@gmail.com>
2017-03-01 14:49:14 +0300

    yeap

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,20 @@
+/**
+ * Created by user on 01.03.2017.
+ */
+
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class CatTest {
+
+    @Test
+    public void testName() throws Exception {
+        assertEquals("Мяу", Cat.voice());
+    }
+//    dfkjdlf
+
+
+}
